96f296ad431b63d52c606f0a929094d794ed993d
devsetgo <mikeryan56@gmail.com>
2024-09-28 03:41:49 +0000

    fixing github link

--- src/bumpcalver/__init__.py
+++ src/bumpcalver/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-DevSetGo CalVer Library
+BumpCalver Library
 =========
 
 Author: Mike Ryan <mikeryan56@gmail.com>
@@ -12,5 +12,4 @@ __version__ = "2024-09-27-007"
 __author__ = "Mike Ryan"
 __license__ = "MIT"
 __copyright__ = f"Copyright© 2024-{date.today().year}"
-__site__ = "https://github.com/devsetgo/dsg-calver"
-
+__site__ = "https://github.com/devsetgo/bumpcalver"
