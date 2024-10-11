78114580bde276590806d5cfb2d1fbe640fcf393
devsetgo <mikeryan56@gmail.com>
2024-10-11 22:55:50 +0000

    Bump version to 2024-10-11-014-beta

--- examples/example.py
+++ examples/example.py
@@ -1,4 +1,4 @@
-__version__ = "2024-10-11-013"
+__version__ = "2024-10-11-014-beta"
 from datetime import datetime
 from zoneinfo import ZoneInfo, available_timezones
 
--- src/bumpcalver/__init__.py
+++ src/bumpcalver/__init__.py
@@ -9,7 +9,7 @@ License: MIT
 """
 from datetime import date
 
-__version__ = "2024-10-11-013"
+__version__ = "2024-10-11-014-beta"
 __author__ = "Mike Ryan"
 __license__ = "MIT"
 __copyright__ = f"Copyright© 2024-{date.today().year}"
