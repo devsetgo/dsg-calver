fdccbeb66c8cda410510e530bd5d73f2b2cc51d9
devsetgo <mikeryan56@gmail.com>
2024-11-09 20:13:56 +0000

    Bump version to 2024-11-09.1.2.3

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 # Variables
 REPONAME = bumpcalver
-APP_VERSION = 2024-11-09.release
+APP_VERSION = 2024-11-09.1.2.3
 PYTHON = python3
 PIP = $(PYTHON) -m pip
 PYTEST = $(PYTHON) -m pytest
--- src/bumpcalver/__init__.py
+++ src/bumpcalver/__init__.py
@@ -9,7 +9,7 @@ License: MIT
 """
 from datetime import date
 
-__version__ = "2024-11-09.release"
+__version__ = "2024-11-09.1.2.3"
 __author__ = "Mike Ryan"
 __license__ = "MIT"
 __copyright__ = f"Copyright© 2024-{date.today().year}"
